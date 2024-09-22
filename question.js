df075bc32c84fe59bf5c33bff0b78805f3c2701f
Sandhiya-Baskar <sandhiyasanthosh93@gmail.com>
2024-09-22 11:47:32 +0530

    Created img2.jpg and plainimg2.jpg and plainimg3.jpg and plainimg4.jpg and plainimg6.jpg

---
+++ question.js
@@ -0,0 +1,57 @@
+let question=[
+    {
+        numb:1,
+        question:"1. Who created Java?",
+        answer:"James Gosling",
+        options:[
+            "a. James Gosling",
+           " b. Bill Gates",
+            "c. Steve Jobs",
+            "d. Elon Musk",
+        ]
+    },
+    {
+        numb:2,
+        question:"2. What is the basic syntax to print output in Java?",
+        answer:"System.out.println()",
+        options:[
+            "a. System.out.print()",
+           " b. System.out.println() ",
+            "c. Console.write()",
+            "d. Print.out()",
+        ]
+    },
+    {
+        numb:3,
+        question:"3. What is the default data type of a variable that stores a whole number in Java?",
+        answer:"int",
+        options:[
+            "a) int",
+            "b) float",
+            "c) double",
+            "d) char",
+        ]
+    },
+    {
+        numb:4,
+        question:"4. What is the operator used for multiplication in Java?",
+        answer:" *",
+        options:[
+            " a) +",
+            " b) -",
+            " c) *",
+            " d) /",
+        ]
+    },
+    {
+        numb:5,
+        question:"5. What is the purpose of the main method in Java?",
+        answer:"To start the program",
+        options:[
+            " a) To declare variables",
+            " b) To print output",
+            " c) To start the program",
+            " d) To end the program ",
+        ]
+    }
+];
