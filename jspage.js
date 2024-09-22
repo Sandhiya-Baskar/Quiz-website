038a9bf1293b263a216744d770e7d8ae09dfdf0c
Sandhiya-Baskar <sandhiyasanthosh93@gmail.com>
2024-09-22 17:54:23 +0530

    Created img2.jpg and plainimg2.jpg and plainimg3.jpg and plainimg4.j6.jpg

--- jspage.js
+++ jspage.js
@@ -24,22 +24,89 @@ continueBtn.onclick= () => {
     quizBox.classList.add('active');
 
     showQuestions(0);
+    questionCounter(1);
+    headerScore();
 }
+
 let questionCount =0;
+let questionNumb =1;
+let userScore = 0;
+
+
 
 const nextBtn = document.querySelector('.next-btn');
 
 nextBtn.onclick= () => { 
-    questionCount++;
-    showQuestions(questionCount);
+    if(questionCount < questions.length - 1){
+       questionCount++;
+       showQuestions(questionCount);  
+
+       questionNumb++;
+       questionCounter(questionNumb);
+
+       nextBtn.classList.remove('active');
+    }
+    else{
+        console.log('Question Completed');
+    }
 }
 
-const optionList = document.querySelector('.option-list');
+const optionList =  document.querySelector('.option-list');
 
-//getting questions and options from array
 function showQuestions(index){
-    const questionText = document.querySelector('.question-text');
-    questionText.textContent = '${questions[index].numb}. ${questions[index].question}';
+    const questionText = document.querySelector('.question-test');
+    questionText.textContent = `${questions[index].numb}.${questions[index].question}`;
 
-    let optionTag = '<div class="option"><span>${questions[index].options[0]}</span></div>
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+         <div class="option"><span>${questions[index].options[1]}</span></div>
+         <div class="option"><span>${questions[index].options[2]}</span></div>
+         <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++){
+        option[i].setAttribute('onclick','optionSelected(this)');
+    }
+
+
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+    if(userAnswer == correctAnswer){
+        
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+    }
+    else{
+        
+        answer.classList.add('incorrect');
+        for (let i=0; i < allOptions; i++){
+            if (optionList.children[i].textContent == correctAnswer){
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+
+    }
+
+    for (let i=0; i < allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+    
+}
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore(){
+    const headerScoreText = document.querySelector('.header-score');
+    eaderScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
 }
--- question.js
+++ question.js
@@ -1,57 +1,57 @@
-let question=[
+let questions=[
     {
         numb:1,
-        question:"1. Who created Java?",
-        answer:"James Gosling",
+        question:" Who created Java?",
+        answer:"a. James Gosling",
         options:[
             "a. James Gosling",
            " b. Bill Gates",
             "c. Steve Jobs",
-            "d. Elon Musk",
+            "d. Elon Musk"
         ]
     },
     {
         numb:2,
-        question:"2. What is the basic syntax to print output in Java?",
-        answer:"System.out.println()",
+        question:" What is the basic syntax to print output in Java?",
+        answer:"b. System.out.println()",
         options:[
             "a. System.out.print()",
            " b. System.out.println() ",
             "c. Console.write()",
-            "d. Print.out()",
+            "d. Print.out()"
         ]
     },
     {
         numb:3,
-        question:"3. What is the default data type of a variable that stores a whole number in Java?",
-        answer:"int",
+        question:" What is the default data type in Java?",
+        answer:"a. int",
         options:[
-            "a) int",
-            "b) float",
-            "c) double",
-            "d) char",
+            "a. int",
+            "b. float",
+            "c. double",
+            "d. char"
         ]
     },
     {
         numb:4,
-        question:"4. What is the operator used for multiplication in Java?",
-        answer:" *",
+        question:" What is the operator used for multiplication in Java?",
+        answer:"c. *",
         options:[
-            " a) +",
-            " b) -",
-            " c) *",
-            " d) /",
+            " a. +",
+            " b. -",
+            " c. *",
+            " d. /"
         ]
     },
     {
         numb:5,
-        question:"5. What is the purpose of the main method in Java?",
-        answer:"To start the program",
+        question:" What is the purpose of the main method in Java?",
+        answer:"c. To start the program",
         options:[
-            " a) To declare variables",
-            " b) To print output",
-            " c) To start the program",
-            " d) To end the program ",
+            " a. To declare variables",
+            " b. To print output",
+            " c. To start the program",
+            " d. To end the program "
         ]
     }
 ];
